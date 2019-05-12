718c8e99dc780d914e5e8c01065549558b61e98d
KrzychuChopin <krzysztof.gomulka@op.pl>
2019-05-12 21:00:15 +0200

    toast massages

--- app/src/main/java/com/abc/ytoddler/Pop.java
+++ app/src/main/java/com/abc/ytoddler/Pop.java
@@ -3,8 +3,14 @@ package com.abc.ytoddler;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Pop extends Activity {
+
+    protected Button girl_four_button, girl_seven_button, girl_ten_button,
+            boy_four_button, boy_seven_button, boy_ten_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,25 @@ public class Pop extends Activity {
         int height = dm.heightPixels;
 
         getWindow().setLayout((int)(width*.8),(int) (height*.6));
+
+        girl_four_button = findViewById(R.id.girl_four_button);
+        girl_seven_button = findViewById(R.id.girl_seven_button);
+        girl_ten_button = findViewById(R.id.girl_ten_button);
+        boy_four_button = findViewById(R.id.boy_four_button);
+        boy_seven_button = findViewById(R.id.boy_seven_button);
+        boy_ten_button = findViewById(R.id.boy_ten_button);
+
+        girl_four_button.setOnClickListener(v -> Toast.makeText(getApplicationContext(), "Video has been added", Toast.LENGTH_SHORT).show());
+
+        girl_seven_button.setOnClickListener(v -> Toast.makeText(getApplicationContext(), "Video has been added", Toast.LENGTH_SHORT).show());
+
+        girl_ten_button.setOnClickListener(v -> Toast.makeText(getApplicationContext(), "Video has been added", Toast.LENGTH_SHORT).show());
+
+        boy_four_button.setOnClickListener(v -> Toast.makeText(getApplicationContext(), "Video has been added", Toast.LENGTH_SHORT).show());
+
+        boy_seven_button.setOnClickListener(v -> Toast.makeText(getApplicationContext(), "Video has been added", Toast.LENGTH_SHORT).show());
+
+        boy_ten_button.setOnClickListener(v -> Toast.makeText(getApplicationContext(), "Video has been added", Toast.LENGTH_SHORT).show());
     }
 
 }
